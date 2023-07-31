7047be93f17b11f6390ca786fc745c1b71e053c1
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2023-07-31 12:52:45 +0530

    first commit

---
+++ Front End Web Development/4 Angular/Angular Routing/Activity 3/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ActivityComponent } from './components/activity/activity.component';
+
+const routes: Routes = [{path:'activity', component : ActivityComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Front End Web Development/4 Angular/Angular Services/Activity 1/src/app/components/products/products.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../Models/product.model';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
+})
+export class ProductsComponent implements OnInit {
+  public rowIndex!: number;
+  showAddProduct!: boolean;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  public products: Product[] = [];
+
+  public selectProduct(selectedRow: number) {
+    this.rowIndex = selectedRow;
+  }
+
+  showAddProducts() {
+    this.showAddProduct = true;
+  }
+
+  hideAddProducts() {
+    this.showAddProduct = false;
+  }
+}
